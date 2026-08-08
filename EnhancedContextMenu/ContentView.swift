a9947bb7fffccfea286f427beae66fb67990baf6
tlb22 <tlb22@mails.tsinghua.edu.cn>
2026-08-08 18:19:50 +0800

    Initial Commit

---
+++ EnhancedContextMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnhancedContextMenu
+//
+//  Created by Billytang on 2026/8/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnhancedContextMenu/EnhancedContextMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedContextMenuApp.swift
+//  EnhancedContextMenu
+//
+//  Created by Billytang on 2026/8/8.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedContextMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
